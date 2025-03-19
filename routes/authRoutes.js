03c59232c1c8beb7433576e2e1b016f7af006d58
RawandAqel <161249085+RawandAqel@users.noreply.github.com>
2025-03-19 05:47:45 +0200

    Create logout API

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { login } = require('../controllers/authController');
 const { signupClient, signupServiceProvider } = require('../controllers/signupController');
+const { logout } = require('../controllers/logoutController');
 const { validateSignup, handleValidationErrors } = require('../middleware/signupValidator');
+const { validateLogout } = require('../middleware/logoutValidator');
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/signup/client', validateSignup, handleValidationErrors, signupClient);
 router.post('/signup/provider', validateSignup, handleValidationErrors, signupServiceProvider);
+router.post('/logout', validateLogout, handleValidationErrors, logout);
 
 module.exports = router;
